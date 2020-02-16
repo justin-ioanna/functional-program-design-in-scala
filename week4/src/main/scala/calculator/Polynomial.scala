283360ec61bdc7a89a399afae5d49655fea3ffcd
Justin Ioanna <justin.ioanna11@outlook.com>
2020-02-16 22:04:56 +0000

    Add week4

---
+++ week4/src/main/scala/calculator/Polynomial.scala
@@ -0,0 +1,28 @@
+package calculator
+
+object Polynomial extends PolynomialInterface {
+  def computeDelta(
+      a: Signal[Double],
+      b: Signal[Double],
+      c: Signal[Double]
+  ): Signal[Double] =
+    Signal {
+      math.pow(b(), 2) - (4 * a() * c())
+    }
+
+  def computeSolutions(
+      a: Signal[Double],
+      b: Signal[Double],
+      c: Signal[Double],
+      delta: Signal[Double]
+  ): Signal[Set[Double]] =
+    Signal {
+      if (delta() < 0) Set()
+      else
+        Set(
+          (-b() + math.sqrt(delta())) / (2 * a()),
+          (-b() - math.sqrt(delta())) / (2 * a())
+        )
+
+    }
+}
